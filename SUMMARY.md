18a3390d8125259fa08ae98ca1ca4b75c5c19716
JustCat3456 <JustCat3456@users.noreply.github.com>
2024-12-12 17:44:07 +0000

    deploy: 73576b22670247e89b84e4b6c992f70b8f014645

--- SUMMARY.md
+++
@@ -1,8 +0,0 @@
-# Summary
-
-* [Introduction](readme.md)
-* [第一回：個体地球](./doc/part1/part1.md)
-* [第二回：岩石鉱物と地形](./doc/part2/part2.md)
-* [第三回：大気と海洋](./doc/part3/part3.md)
-* [第四回：宇宙](./doc/part4/part4.md)
-
--- _book/mathjax.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  book: {
-    assets: './assets',  // 静的ファイルを置く場所を指定
-    js: [
-      'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js' // CDNからMathJaxを読み込み
-    ]
-  }
-};
